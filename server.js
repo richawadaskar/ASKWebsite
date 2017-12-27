72d87b12376cdc32a2e293a1e47272cddd64c596
richawadaskar <richa.wadaskar@gmail.com>
2017-12-26 23:11:50 -0800

    Adding some routes to server.js

---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to append to server.log.')
+		}
+	});
+	next();
+});
+
+// app.use((req, res, next) => {
+// 	res.render('maintanance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Richard\'s Home Page',
+		welcomeMessage: 'Welcome home Richard fans. Get ready for an adventure!'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});	
+});
+
+app.get('/values', (req, res) => {
+	res.render('values.hbs', {
+		pageTitle: 'About Page'
+	});	
+});
+
+app.get('/recruitment', (req, res) => {
+	res.render('recruitment.hbs', {
+		pageTitle: 'About Page'
+	});	
+});
+
+app.get('/members', (req, res) => {
+	res.render('members.hbs', {
+		pageTitle: 'About Page'
+	});	
+});
+
+
+app.get('/connect', (req, res) => {
+	res.render('connect.hbs', {
+		pageTitle: 'About Page'
+	});	
+});
+
+app.get('/gallery', (req, res) => {
+	res.render('gallery.hbs', {
+		pageTitle: 'About Page'
+	});	
+});
+
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
+});
